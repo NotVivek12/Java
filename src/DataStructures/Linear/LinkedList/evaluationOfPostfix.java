2d0be0cdc7dbb754406bcdbd87f1af8a20df39e4
notvivek12 <vivekjoshi82015@gmail.com>
2024-09-20 14:36:46 +0530

    Evaluation of postfix expression

---
+++ src/DataStructures/Linear/LinkedList/evaluationOfPostfix.java
@@ -0,0 +1,45 @@
+package DataStructures.Linear.LinkedList;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class evaluationOfPostfix {
+
+    public static int evaluatePostfix(String s) {
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == ' ') {
+                continue;
+            } else if(Character.isDigit(ch)) {
+                st.push(ch-'0');
+            }
+            else {
+                int b = st.pop();
+                int a = st.pop();
+
+                if (ch == '+') {
+                    int c = a+b;
+                    st.push(c);
+                } else if (ch == '-') {
+                    int c = a-b;
+                    st.push(c);
+                } else if (ch == '*') {
+                    int c = a*b;
+                    st.push(c);
+                } else if (ch == '/') {
+                    int c = a/b;
+                    st.push(c);
+                }
+            }
+        }
+        return st.pop();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int result = evaluatePostfix(s);
+        System.out.println(result);
+    }
+}
